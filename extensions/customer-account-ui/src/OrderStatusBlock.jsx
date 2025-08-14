e5f5ecaaa00f4b687f86069ce0715af633b2686e
rohit110202 <rohitpatel@itgeeks.com>
2025-08-14 17:46:40 +0530

    invoice download

---
+++ extensions/customer-account-ui/src/OrderStatusBlock.jsx
@@ -0,0 +1,63 @@
+import {
+  reactExtension,
+  useApi,
+  Button,
+  Link,
+} from "@shopify/ui-extensions-react/customer-account";
+import { useState, useEffect } from "react";
+
+export default reactExtension(
+  "customer-account.order-status.block.render",
+  () => <PromotionBanner />
+);
+
+function PromotionBanner() {
+  const { order, cost, lines } = useApi();
+  const [pdfUrl, setPdfUrl] = useState("");
+  console.log(order?.current?.id);
+  let orderId = order?.current?.id || "Unknown";
+  orderId = orderId.replace("gid://shopify/Order/", "");
+  const orderData = {
+    order_id: orderId,
+  };
+
+  useEffect(() => {
+    getPdfLink();
+  }, []); // Run once when component mounts
+
+  async function getPdfLink() {
+    try {
+      const response = await fetch(
+        "https://custom-app.accounteditor.com/custom_requirement/sunberryrx/pdflink.php",
+        
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(orderData),
+        }
+      );
+
+      const result = await response.json();
+      if (result.success && result.pdf_link) {
+        setPdfUrl(result.pdf_link); // Update state with PDF URL
+      } else {
+        console.error("Failed to generate PDF:", result.message);
+      }
+    } catch (error) {
+      console.error("Error fetching PDF:", error);
+    }
+  }
+
+  return (
+    <>
+
+      {pdfUrl && <Link to={pdfUrl} external={true}>
+        <Button  >
+          Download Invoice
+        </Button>
+      </Link>}
+    </>
+  );
+}
